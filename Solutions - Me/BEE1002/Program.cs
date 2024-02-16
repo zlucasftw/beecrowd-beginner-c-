ee2567b477012de182bb83a130ec9779e1c74e20
zlucasftw <ilucasmcpvp@gmail.com>
2024-02-16 06:45:43 -0300

    commit: BEE1002
    Solution for BEE1002 problem.

---
+++ Solutions - Me/BEE1002/Program.cs
@@ -0,0 +1,17 @@
+﻿using System; 
+
+class BEE1002 {
+
+    static void Main(string[] args) {
+        
+        float R = float.Parse(Console.ReadLine());
+
+        R = toString(4);
+        
+        float n = 3.14159f;
+        
+        float A = n * (R * R);
+        
+        Console.WriteLine($"A={A}");
+    }
+}
